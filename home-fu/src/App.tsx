7e764ae23a2cfae4462beb4e2cd79fd479f4c1dc
Danila <113025911+Danila2510@users.noreply.github.com>
2025-03-02 18:41:12 +0200

    Created button register

--- home-fu/src/App.tsx
+++ home-fu/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import MapButton from "./components/Header/MapButton/mapbutton";
+import RegisterButton from "./components/Header/RegisterButton/registerbutton";
 
 function App() {
   return (
     <div className="container">
-     
+      <MapButton />
+      <RegisterButton />
     </div>
   );
 }
