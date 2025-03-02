dae7f036794d0fb77e0afd0859e5b25d87efb938
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-03-02 17:50:58 +0200

    added normalize styles

--- home-fu/src/App.tsx
+++ home-fu/src/App.tsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import MapButton from "./components/Header/MapButton/mapbutton";
 
 function App() {
   return (
     <div className="container">
-      <MapButton/>
+     
     </div>
   );
 }
