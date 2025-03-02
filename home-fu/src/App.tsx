df78c4756a13b45eb8d2a2295cb7e9788ce00f3e
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-03-02 19:45:16 +0200

    navbar menu first step

--- home-fu/src/App.tsx
+++ home-fu/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/header";
 function App() {
   return (
     <div className="container">
-     
+      <Header></Header>
     </div>
   );
 }
--- home-fu/src/components/Header/header.tsx
+++ home-fu/src/components/Header/header.tsx
@@ -1,7 +1,20 @@
+import style from "./header.module.scss";
+
 const Header = () => {
-    return (<>
-    
-    </>)
+    return (
+        <header className={style.header}>
+            <div className={style.content}>
+                <div className={style.logo}><a>HomeFU</a></div>
+                <ul className={style.listMenu}>
+                    <li className={style.listItem}><a>Варіанти помешкань</a></li>
+                    <li className={style.listItem}><a>Враження</a></li>
+                    <li className={style.listItem}><a>Онлайн-враження</a></li>
+                </ul>
+                <div>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
