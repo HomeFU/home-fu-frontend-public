d6a2e6030d7f66f94a31a741c165576787c10fcd
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-03-04 18:13:21 +0200

    created index page

--- home-fu/src/App.tsx
+++ home-fu/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/header";
-import Footer from "./components/Footer/footer";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="container">
-      <Header></Header>
-      <Footer></Footer>
+      <Home></Home>
     </div>
   );
 }
---
+++ home-fu/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
+
+const Home = () => {
+
+    return (<>
+        <Header></Header>
+        <main>
+            <h1>Home Page</h1>
+        </main>
+        <Footer></Footer>
+    </>)
+}
+
+export default Home;
