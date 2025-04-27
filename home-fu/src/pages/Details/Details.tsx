42ddef1f95d56159d574b9748e793972e23a7e84
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-04-27 21:51:17 +0300

    feature/created base layout

--- home-fu/src/pages/Details/Details.tsx
+++ home-fu/src/pages/Details/Details.tsx
@@ -1,7 +1,17 @@
 import style from "./details.module.scss"
-
+import { Header } from "../../components/Header/header"
+import { Footer } from "../../components/Footer/footer"
 export const Details = () => {
-    return (<>
-        <h1 className={style.h1}>This is Page Detais</h1>
-    </>)
+    return (
+        <>
+         <Header></Header>
+         <main className={style.main}>
+            <div className={style.divider}></div>
+            <div className={style.container}>
+                <h1>Hello , this is Page Details</h1>
+            </div>
+         </main>
+         <Footer></Footer>
+        </>
+    )
 }
