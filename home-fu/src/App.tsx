8c6b6695d47b16cedcd3acaa32710db79213ac46
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-04-29 13:39:06 +0300

    feature/create profile page

--- home-fu/src/App.tsx
+++ home-fu/src/App.tsx
@@ -2,12 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import { Details } from "./pages/Details/Details";
-
+import { Profile } from "./pages/Profile/Profile";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
         <Route path="/details" element={<Details/>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ home-fu/src/pages/Profile/Profile.tsx
@@ -0,0 +1,7 @@
+import style from "./profile.module.scss";
+
+export const Profile = () => {
+    return (
+        <h1 className={style.title}>This page Profile</h1>
+    )
+}
