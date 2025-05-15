89e8c12dcf1b42a46db58e359c6396afb241a11c
Danila <113025911+Danila2510@users.noreply.github.com>
2025-05-15 17:28:24 +0300

    added coming soon

--- home-fu/src/components/MenuPopUp/menuPopUp.tsx
+++ home-fu/src/components/MenuPopUp/menuPopUp.tsx
@@ -13,19 +13,19 @@ export const MenuPopoUp = () => {
                 openMenuPopoUp && (
                     <div className={style.wrapperPopUp}>
                         <ul className={`${style.listLinks} ${style.listLinksFirstBlock}`}>
-                            <li className={style.listLinksItem}><Link to="/">Повідомлення</Link></li>
-                            <li className={style.listLinksItem}><Link to="/">Сповіщення</Link></li>
-                            <li className={style.listLinksItem}><Link to="/">Подорожі</Link></li>
-                            <li className={style.listLinksItem}><Link to="/">Обране</Link></li>
+                            <li className={style.listLinksItem}><Link to="/">Повідомлення</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
+                            <li className={style.listLinksItem}><Link to="/">Сповіщення</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
+                            <li className={style.listLinksItem}><Link to="/">Подорожі</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
+                            <li className={style.listLinksItem}><Link to="/">Обране</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
                         </ul>
                         <ul className={`${style.listLinks} ${style.listLinksSecondBlock}`}>
-                            <li className={style.listLinksItem}><Link to="/">Запропонувати помешкання на Home<span className={style.logoModifier}>FU</span></Link></li>
-                            <li className={style.listLinksItem}><Link to="/">Організувати враження</Link></li>
-                            <li className={style.listLinksItem}><Link to="/">Запросити господаря</Link></li>
+                            <li className={style.listLinksItem}><Link to="/">Запропонувати помешкання на Home<span className={style.logoModifier}>FU</span></Link><div className={style.comingSoonPopup}>Coming soon</div></li>
+                            <li className={style.listLinksItem}><Link to="/">Організувати враження</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
+                            <li className={style.listLinksItem}><Link to="/">Запросити господаря</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
                             <li className={style.listLinksItem}><Link to="/profile">Ваш профіль</Link></li>
                         </ul>
                         <ul className={`${style.listLinks} ${style.listLinksThirdBlock}`}>
-                            <li className={style.listLinksItem}><Link to="/">Довідковий центр</Link></li>
+                            <li className={style.listLinksItem}><Link to="/">Довідковий центр</Link><div className={style.comingSoonPopup}>Coming soon</div></li>
                             <li className={style.listLinksItem}><LogOutButton/></li>
                         </ul>
                     </div>
