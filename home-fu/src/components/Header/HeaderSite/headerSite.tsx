4508a9f17a411a6cd204fa49ed27907e8a7ce4ca
Danila <113025911+Danila2510@users.noreply.github.com>
2025-05-17 22:50:43 +0300

    fix profile

--- home-fu/src/components/Header/HeaderSite/headerSite.tsx
+++ home-fu/src/components/Header/HeaderSite/headerSite.tsx
@@ -1,22 +1,21 @@
 "use client"
 
 import style from "./headerSite.module.scss"
-import { RegisterButton } from "..//RegisterButton/registerbutton"
+import { RegisterButton } from "../RegisterButton/registerbutton"
 import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
-import { LogOutButton } from "..//../../features/Auth/LogOut/logOutButton"
 import { openCloseMobileMenu } from "../../../redux/MobileMenu/menuSlice"
 import { MobileMenu } from "../../MobileMenu/menu"
+import { AuthenticatedUserButton } from "../../../features/Auth/ButtonForAuthenticatedUser/authenticatedUserButton"
+import { MenuPopoUp } from "../../MenuPopUp/menuPopUp"
 
 export const HeaderSite = () => {
-  const dispatch = useDispatch();
-
-  const isOpenMobileMenu = useSelector((state) => state.mobileMenu.isOpen);
-
+  const dispatch = useDispatch()
+  const isOpenMobileMenu = useSelector((state) => state.mobileMenu.isOpen)
   const isAuthenticatedUser = useSelector((state) => state.auth.isAuthenticated)
 
   const onOpenCloceMobileMenu = () => {
-    dispatch(openCloseMobileMenu());
+    dispatch(openCloseMobileMenu())
   }
 
   return (
@@ -28,27 +27,33 @@ export const HeaderSite = () => {
               <Link to="/">
                 Home<span className={style.logoModifier}>FU</span>
               </Link>
-            </div> 
+            </div>
             <div className={style.rightSection}>
               <Link className={style.offerItem} to="/">
                 Запропонувати помешкання на Home<span className={style.logoModifier}>FU</span>
               </Link>
-              <div className={style.userMenu}>{isAuthenticatedUser ? <LogOutButton /> : <RegisterButton />}</div>
+              <div className={style.userMenu}>
+                {isAuthenticatedUser ? (
+                  <div className={style.userMenuWrapper}>
+                    <AuthenticatedUserButton />
+                    <MenuPopoUp />
+                  </div>
+                ) : (
+                  <RegisterButton />
+                )}
+              </div>
             </div>
-            <button
-                className={style.menuButton}
-                onClick={onOpenCloceMobileMenu}
-                >
-                <div className={style.menuIcon}>
-                    <span className={isOpenMobileMenu ? style.open : ''} />
-                    <span className={isOpenMobileMenu ? style.open : ''} />
-                    <span className={isOpenMobileMenu ? style.open : ''} />
-                </div>
+            <button className={style.menuButton} onClick={onOpenCloceMobileMenu}>
+              <div className={style.menuIcon}>
+                <span className={isOpenMobileMenu ? style.open : ""} />
+                <span className={isOpenMobileMenu ? style.open : ""} />
+                <span className={isOpenMobileMenu ? style.open : ""} />
+              </div>
             </button>
           </div>
         </div>
       </header>
-      <MobileMenu/>
+      <MobileMenu />
     </>
   )
 }
