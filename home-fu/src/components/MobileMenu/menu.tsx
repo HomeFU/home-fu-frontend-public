c5c1e154421933f0418cc38a151eec7158ea7ce3
Danila <113025911+Danila2510@users.noreply.github.com>
2025-05-20 19:41:15 +0300

    fixed link

--- home-fu/src/components/MobileMenu/menu.tsx
+++ home-fu/src/components/MobileMenu/menu.tsx
@@ -12,18 +12,17 @@ export const MobileMenu = () => {
         <div className={`${style.menu} ${isOpenMobileMenu ? style.openMenu : ''}`}>
             <div className={style.content}>
                 <ul className={style.listMenu}>
-                    <li className={style.listItem}><Link to="/">Варіанти помешкань</Link></li>
-                    <li className={style.listItem}><Link to="/">Враження</Link></li>
-                    <li className={style.listItem}><Link to="/">Онлайн-враження</Link></li>
-                    <li className={style.listItem}><Link to="/">Повідомлення</Link></li>
-                    <li className={style.listItem}><Link to="/">Сповіщення</Link></li>
-                    <li className={style.listItem}><Link to="/">Подорожі</Link></li>
-                    <li className={style.listItem}><Link to="/">Обране</Link></li>
-                    <li className={style.listItem}><Link to="/">Запропонувати помешкання на Home<span className={style.logoModifier}>FU</span></Link></li>
-                    <li className={style.listItem}><Link to="/">Організувати враження</Link></li>
-                    <li className={style.listItem}><Link to="/">Запросити господаря</Link></li>
+                    <li className={style.listItem}><p>Варіанти помешкань</p></li>
+                    <li className={style.listItem}><p>Враження</p></li>
+                    <li className={style.listItem}><p>Онлайн-враження</p></li>
+                    <li className={style.listItem}><p>Повідомлення</p></li>
+                    <li className={style.listItem}><p>Сповіщення</p></li>
+                    <li className={style.listItem}><p>Подорожі</p></li>
+                    <li className={style.listItem}><p>Обране</p></li>
+                    <li className={style.listItem}><p>Організувати враження</p></li>
+                    <li className={style.listItem}><p>Запросити господаря</p></li>
                     <li className={style.listItem}><Link to="/profile">Ваш профіль</Link></li>
-                    <li className={style.listItem}><Link to="/">Довідковий центр</Link></li>
+                    <li className={style.listItem}><p>Довідковий центр</p></li>
                 </ul>
                 <div className={style.offerListWrapper}>
                     <Link className={style.offerItem} to="/">Запропонувати помешкання на Home<span className={style.logoModifier}>FU</span></Link>
