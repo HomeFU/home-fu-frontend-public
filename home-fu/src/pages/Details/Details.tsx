4c24c49c5bdce22b129813e44b6f3dd4c72661c7
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-06-10 19:03:46 +0300

    added fields

--- home-fu/src/pages/Details/Details.tsx
+++ home-fu/src/pages/Details/Details.tsx
@@ -65,6 +65,15 @@ export const Details = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-map-pin w-4 h-4"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>
                                         <span className={style.locationName}>{data?.card.locationName}</span>
                                     </div>
+                                    <div className={style.wrapperPrice}>
+                                        <span>Price: {data?.card.price}$</span>
+                                    </div>
+                                    <div className={style.wrapperPrice}>
+                                        <span>Days: </span>
+                                        <span>{data?.card.startDate ? new Date(data.card.startDate).getDate() : ''}</span>
+                                        -
+                                        <span>{data?.card.endDate ? new Date(data.card.endDate).getDate() : ''}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
