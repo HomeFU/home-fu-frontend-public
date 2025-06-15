ef75269c7e1950cf6ad6e817d151c19e6b1b836c
Petrik <112963523+petrikpopov@users.noreply.github.com>
2025-06-15 22:32:03 +0300

    first step

--- home-fu/src/pages/Details/Details.tsx
+++ home-fu/src/pages/Details/Details.tsx
@@ -233,6 +233,39 @@ export const Details = () => {
                             </div>
                         </div>
                     </div>
+                    <div className={style.commentGuest}>
+                        <div className={style.wrapperCommentGuestHead}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sparkles w-5 h-5 text-blue-500"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg>
+                            <h3>Відгукі гостей</h3>
+                        </div>
+                        <div className={style.wrapperCommentGuest}>
+                            {
+                                data?.reviews.map((el) => (
+                                    <div className={style.commentItem}>
+                                        <div className={style.commentItemHead}>
+                                            <div className={style.commentItemBlockImg}><img src={`https://homefu.azurewebsites.net${el.userProfileImageUrl}`} alt="userProfileImage" /></div>
+                                            <div className={style.wrapperNameStar}>
+                                                <span>{el.userName}</span>
+                                                <span>
+                                                    {Number(el.overallRating) > 0 &&
+                                                        Array.from({ length: Number(el.overallRating) }, (_, i) => (
+                                                            <svg key={i} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-star w-3 h-3 text-blue-400 fill-blue-400"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                                                        ))
+                                                    }
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className={style.wrapperCreatedAt}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-calendar _7QGNqa_metaIcon"><path d="M8 2v4"></path><path d="M16 2v4"></path><rect width="18" height="18" x="3" y="4" rx="2"></rect><path d="M3 10h18"></path></svg>
+                                            <span>{el.createdAt}</span>
+                                        </div>
+                                        <div>{el.text}</div>
+                                        <div><span>Загальний рейтинг: {el.overallRating}</span></div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>  
                     </div>
                         </>
                     }
